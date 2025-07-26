23e541f2af501cf061f2a648e7f55148303db6af
Walter Moreira <walter@waltermoreira.net>
2025-07-25 21:21:27 -0500

    Tag operators as reducible

--- lean/gen-seq/GenSeq/Semantics.lean
+++ lean/gen-seq/GenSeq/Semantics.lean
@@ -21,62 +21,81 @@ def mod (a : ℤ) (b : ℤ) : ℤ := a % b
 @[simp]
 def cond (a b c : ℤ) : ℤ := if a ≤ 0 then b else c
 
-@[simp]
-def loop (f : ℤ → ℤ → ℤ) (a : ℕ) (b : ℤ) : ℤ :=
+@[simp,reducible]
+def loop (f : ℤ → ℤ → ℤ) (a : ℤ) (b : ℤ) : ℤ :=
   match a with
-  | 0 => b
-  | n + 1 => f (loop f n b) (n + 1)
+  | .ofNat 0 => b
+  | .ofNat (n + 1) => f (loop f n b) (n + 1)
+  | .negSucc _ => b
+termination_by Int.natAbs a
 
-@[simp]
-def loop2 (f g : ℤ → ℤ → ℤ) (a : ℕ) (b c : ℤ) : ℤ :=
+@[simp,reducible]
+def loop2 (f g : ℤ → ℤ → ℤ) (a : ℤ) (b c : ℤ) : ℤ :=
   match a with
-  | 0 => b
-  | n + 1 =>
-    loop2 f g n (f b c) (g b c)
+  | .ofNat 0 => b
+  | .ofNat (n + 1) => loop2 f g n (f b c) (g b c)
+  | .negSucc _ => b
+termination_by Int.natAbs a
 
-@[simp]
-def loop2' (f g : ℤ × ℤ → ℤ) (a : ℕ) (b : ℤ × ℤ) : ℤ × ℤ :=
+@[simp,reducible]
+def loop2' (f g : ℤ × ℤ → ℤ) (a : ℤ) (b : ℤ × ℤ) : ℤ × ℤ :=
   match a with
-  | 0 => b
-  | n + 1 =>
-    loop2' f g n (f b, g b)
+  | .ofNat 0 => b
+  | .ofNat (n + 1) => loop2' f g n (f b, g b)
+  | .negSucc _ => b
+termination_by Int.natAbs a
 
-theorem loop2_eq_loop2' (f g : ℤ → ℤ → ℤ) (a : ℕ) (b c : ℤ) :
+theorem loop2_eq_loop2' (f g : ℤ → ℤ → ℤ) (a : ℤ) (b c : ℤ) :
     loop2 f g a b c = (loop2' (uncurry f) (uncurry g) a (b, c)).1 := by
-  induction a generalizing b c with
-  | zero => simp
-  | succ _ ih => simp; exact ih _ _
+  match a with
+  | .negSucc _ => reduce; rfl
+  | .ofNat a =>
+    simp
+    induction a generalizing b c with
+    | zero => reduce; rfl
+    | succ _ ih =>
+      simp
+      unfold loop2 loop2'
+      exact ih _ _
 
 theorem loop2'_rec (f g : ℤ × ℤ → ℤ) (n : ℕ) (b : ℤ × ℤ) :
     loop2' f g (n + 1) b = (f (loop2' f g n b), g (loop2' f g n b)) := by
   induction n generalizing b with
-  | zero => simp
+  | zero => reduce; rfl
   | succ m ih => unfold loop2'; exact ih _
 
+example (n : ℕ) : Int.ofNat (n + 1) = n + 1 := rfl
+
 @[simp]
-theorem loop2_out (f g : ℤ → ℤ → ℤ) (a : ℕ) (b c : ℤ) :
+theorem loop2_out (f g : ℤ → ℤ → ℤ) (a : ℤ) (b c : ℤ) :
     loop2 f g a b c = b ∨ ∃ b' c', loop2 f g a b c = f b' c' := by
   match a with
-  | 0 => left; simp
-  | n + 1 =>
-    right
-    rw [loop2_eq_loop2', loop2'_rec]
+  | .negSucc _ => reduce; left; rfl
+  | .ofNat 0 => reduce; left; rfl
+  | .ofNat (n + 1) =>
+    rw [loop2_eq_loop2', show Int.ofNat (n + 1) = n + 1 by rfl, loop2'_rec]
     let (u, v) := loop2' (uncurry f) (uncurry g) n (b, c)
+    right
     use u, v
     simp
 
 def N : ℕ := 1000
 
-def comprN (f : ℤ → ℤ) (a : ℕ) : ℤ :=
+@[reducible]
+def comprN (f : ℤ → ℤ) (a : ℤ) : ℤ :=
   match a with
-  | 0 => List.range N |>.find? (f · ≤ 0) |>.getD N
-  | n + 1 => List.range N |>.find? (fun (i : ℕ) => comprN f n < i ∧ f i ≤ 0) |>.getD N
+  | .negSucc _ => 0
+  | .ofNat 0 => List.range N |>.find? (f · ≤ 0) |>.getD N
+  | .ofNat (n + 1) => List.range N |>.find? (fun (i : ℕ) => comprN f n < i ∧ f i ≤ 0) |>.getD N
+termination_by Int.natAbs a
 
-def comprP (f : ℤ → ℤ) (b : ℤ → ℕ) (hb : ∀ n : ℕ, n < b n ∧ f (b n) ≤ 0) (a : ℕ) : ℤ :=
+def comprP (f : ℤ → ℤ) (b : ℤ → ℕ) (hb : ∀ n : ℕ, n < b n ∧ f (b n) ≤ 0) (a : ℤ) : ℤ :=
   match a with
-  | 0 => List.range (b 0) |>.find? (f · ≤ 0) |>.getD (b 0)
-  | n + 1 =>
+  | .negSucc _ => 0
+  | .ofNat 0 => List.range (b 0) |>.find? (f · ≤ 0) |>.getD (b 0)
+  | .ofNat (n + 1) =>
     let N := b (comprP f b hb n)
     List.range N |>.find? (fun (i : ℕ) => comprP f b hb n < i ∧ f i ≤ 0) |>.getD N
+termination_by Int.natAbs a
 
 end Synth
