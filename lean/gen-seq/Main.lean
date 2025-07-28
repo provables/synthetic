920baa9e14dd9ba90b82a97e3400000e51c80769
Walter Moreira <walter@waltermoreira.net>
2025-07-28 16:10:03 -0500

    Finish basic server for genseq

--- lean/gen-seq/Main.lean
+++ lean/gen-seq/Main.lean
@@ -13,9 +13,9 @@ def runExcept {e a : Type} {m : Type → Type} [Monad m] (x : Except e (m (Excep
 
 def process_json (obj : Json) : TermElabM (Except String Json) := do
   let data := do
-    let name ← obj.getObjValAs? String "name"
-    let offst ← obj.getObjValAs? Nat "offset"
-    let source ← obj.getObjValAs? String "source"
+    let name ← obj.getObjValAs? String "name" |>.mapError (s!"missing name: {·}")
+    let offst ← obj.getObjValAs? Nat "offset" |>.mapError (s!"missing offset: {·}")
+    let source ← obj.getObjValAs? String "source" |>.mapError (s!"missing source: {·}")
     let x := DSLToLean name source offst >>= (fun o =>
       let u := o.map (fun v =>
         let j := Json.mkObj [
@@ -39,13 +39,6 @@ def process_data (input : String) : TermElabM String := do
     ]
   return s!"{y}"
 
-run_elab do
-  let x ← process_data r#"{"name": "foo", "offset": 2, "source": "x"}"#
-  dbg_trace s!"{x}"
-  -- let y ← f
-  -- dbg_trace s!"{y}"
-  -- dbg_trace "after"
-
 def process_client (socket : Internal.UV.TCP.Socket) : TermElabM UInt32 := do
   while true do
     let d ← socket.recv? 65536
@@ -59,31 +52,12 @@ def process_client (socket : Internal.UV.TCP.Socket) : TermElabM UInt32 := do
         match String.fromUTF8? u with
         | some text =>
           IO.println s!"got data: {text}"
-
-          match process_data text with
-          | .error s =>
-            IO.println s!"error while processing: {s}"
+          let output ← process_data text
+          match (← socket.send <| String.toUTF8 output).result!.get with
+          | .ok _ => return none
+          | .error e =>
+            IO.println s!"got error while writing: {e}"
             return (some 1)
-          | .ok s =>
-            match (← socket.send <| String.toUTF8 s).result!.get with
-            | .ok _ => return none
-            | .error e =>
-              IO.println s!"got error while writing: {e}"
-              return (some 1)
-
-          -- let parsed ← parse text
-          -- match parsed with
-          -- | .error s =>
-          --   IO.println s!"got parser error: {s}"
-          --   return (some 1)
-          -- | .ok s =>
-          --   let response := String.toUTF8 <| TtoLean "foo" 0 s
-          --   let o := (← socket.send response).result!
-          --   match o.get with
-          --   | .ok _ => return none
-          --   | .error e =>
-          --     IO.println s!"got error while writing: {e}"
-          --     return (some 1)
         | none =>
           IO.println "got data but not utf8"
           return (some 1)
@@ -95,20 +69,20 @@ def process_client (socket : Internal.UV.TCP.Socket) : TermElabM UInt32 := do
       return x
   return 0
 
-def run_server : TermElabM UInt32 := do
+def run_server (port : Nat) : TermElabM UInt32 := do
   let socket ← Internal.UV.TCP.Socket.new
   let addr := IPv4Addr.ofString "0.0.0.0" |>.getD default
-  let endpoint := SocketAddress.v4 {addr := addr, port := 1234}
+  let endpoint := SocketAddress.v4 {addr := addr, port := port}
   socket.bind endpoint
   socket.listen 1
-  IO.println "Ready"
+  IO.println s!"Ready on port {port}"
   while true do
     let conn ← socket.accept
     let result := conn.result!
     let y ← Task.get <| result.map (fun t => do
       match t with
       | .ok s =>
-        IO.println "got connection"
+        IO.println "client connected"
         let x ← process_client s
         IO.println "client finished"
         return x
@@ -122,10 +96,6 @@ def run_server : TermElabM UInt32 := do
 
 unsafe
 def run (p : Parsed) : IO UInt32 := do
-  let name := p.positionalArg! "name" |>.as! String
-  let offst := p.positionalArg! "offset" |>.as! Nat
-  let source := p.positionalArg! "source" |>.as! String
-
   let modules := #[`GenSeq]
   enableInitializersExecution
   initSearchPath (← findSysroot)
@@ -133,19 +103,24 @@ def run (p : Parsed) : IO UInt32 := do
   let ctx : Core.Context := {fileName := "", fileMap := default}
   let state : Core.State := {env}
 
+  let port := p.flag? "port" |>.map (·.as! Nat) |>.getD 8000
   let z ← Prod.fst <$> (Meta.MetaM.toIO · ctx state) do
-    TermElabM.run' run_server
+    TermElabM.run' (run_server port)
   return z
 
 unsafe
 def cmd : Cmd := `[Cli|
   "genseq" VIA run; ["0.1.0"]
-  "Generate a Lean definition from the synthetic DSL."
+  r#"Generate a Lean definition from the synthetic DSL.
 
-  ARGS:
-      name : String;    "Name for the declaration of the sequence"
-      offset : Nat;     "Offset for the sequence"
-      source : String;  "Source code from the synthetic DSL"
+  Requests: {"name": String, "offset": Nat, "source": String}
+  Responses:
+    - {"status": true, "lean": String}
+    - {"status": false, "error": String}
+  "#
+
+  FLAGS:
+    p, port : Nat;      "Listen at port <port> (default: 8000)"
 ]
 
 unsafe
