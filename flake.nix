218294ff3b9d500d22bb875bcd3325aad609368e
Walter Moreira <walter@waltermoreira.net>
2026-03-21 18:17:44 -0500

    Add linux hash

--- flake.nix
+++ flake.nix
@@ -120,7 +120,7 @@
               "aarch64-darwin" = "";
               "aarch64-linux" = "";
               "x86_64-darwin" = "";
-              "x86_64-linux" = "";
+              "x86_64-linux" = "sha256-N1jcHgmwBfyRZsAS2Dn/velTcxmIGh/4PeQideTbzjw=";
             };
           in
           pkgs.stdenv.mkDerivation {
